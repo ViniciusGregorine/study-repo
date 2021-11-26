e3f81cc5bd883eccc54f5fa02cd53788882ef138
Vinicius Gregorine <46830008+ViniciusGregorine@users.noreply.github.com>
2021-11-25 23:34:35 -0300

    Update stack.test.js

--- test-tdd/test/stack.test.js
+++ test-tdd/test/stack.test.js
@@ -7,19 +7,16 @@ describe('My stack', () => {
         stack = new Stack();
     })
 
-
     it('is created empty', () => {
         expect(stack.top).toBe(-1)
         expect(stack.items).toEqual({})
     })
-
 
     it('can push to the top', () => {
         stack.push('fizz')
         expect(stack.top).toBe(0)
         expect(stack.peek).toBe('fizz')
     })
-
 
     it('can pop off', () => {
         stack.push('buzz')
